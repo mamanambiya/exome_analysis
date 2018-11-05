36b99818ebfab263e3cdef76922c66c2f207357c
Mamana <mypandos@gmail.com>
2018-11-05 15:53:13 +0200

    Initial

---
+++ CHPC/exome_analysis_nextflow_CHPC (1).sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -q UCTlong
+#PBS -l nodes=1:ppn=1
+#PBS -j oe
+#PBS -o /mnt/lustre/users/mmbiyavanga/exome_aibst/LOG/exome_analysis.out
+#PBS -M mbymam001@myuct.ac.za
+#PBS -m ae
+
+
+TODAY=$(date +"%Y_%m_%d__%H_%M_%S")
+HOMEDIR="/home/mmbiyavanga/exome_aibst"
+OUTDIR="/mnt/lustre/users/mmbiyavanga/exome_aibst"
+NAME="exome_analysis_nextflow_CHPC"
+
+. $HOME/.bashrc
+
+## Activate environment
+source activate ngs
+
+
+## Nextflowscript here
+cd ${HOMEDIR}
+#cd ${OUTDIR}
+nextflow -log ${OUTDIR}/LOG/exome_analysis.nextflow.log \
+    run ${HOMEDIR}/exome_analysis.nf \
+    -c ${HOMEDIR}/${NAME}.config \
+    -w ${OUTDIR}/work \
+    -resume
+#    -profile pbs
+#   -with-trace ${OUTDIR}/LOG/${NAME}.txt \
+#    -with-timeline ${OUTDIR}/LOG/${NAME}.html \
+#    -with-dag ${OUTDIR}/LOG/${NAME}.dot \
